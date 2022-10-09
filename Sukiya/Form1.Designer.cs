ece048b544d91181e86b25c6eef98d1795d42959
THUY PHUONG <THUY PHUONG@LAPTOP-0NHB81HJ>
2022-10-09 20:06:18 +0700

    Add project files.

---
+++ Sukiya/Form1.Designer.cs
@@ -0,0 +1,349 @@
+﻿namespace Sukiya
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.fluentDesignFormControl1 = new DevExpress.XtraBars.FluentDesignSystem.FluentDesignFormControl();
+            this.fluentFormDefaultManager1 = new DevExpress.XtraBars.FluentDesignSystem.FluentFormDefaultManager(this.components);
+            this.fdfcTrangHienThi = new DevExpress.XtraBars.FluentDesignSystem.FluentDesignFormContainer();
+            this.ceQuanLiNhanVien = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            this.ceThemNhanVien = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            this.ceSuaNhanVien = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            this.ceXoaNhanVien = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            this.accordionControl1 = new DevExpress.XtraBars.Navigation.AccordionControl();
+            this.ceTrangChu = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            this.ceDanhMucMonAn = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            this.ceThemMon = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            this.ceSuaMon = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            this.ceXoaMon = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            this.ceBaoCaoDoanhThu = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            this.ceBaoCaoDT = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            this.ceXemDT = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            this.ceQuanLiKhoHang = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            this.ceBangKiemHang = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            this.ceSPLoss = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            this.ceBangXinNhapHang = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            this.ceLichLam = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            this.ceThemLichLam = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            this.ceSuaXoaLich = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            this.ceTinhLuong = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            this.ceLienHe = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            this.ceGioiThieu = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            this.ceUpdate = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            this.ceDangXuat = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            this.accordionControlElement4 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            this.accordionControlElement3 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            this.accordionControlElement2 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            ((System.ComponentModel.ISupportInitialize)(this.fluentDesignFormControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fluentFormDefaultManager1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.accordionControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fluentDesignFormControl1
+            // 
+            this.fluentDesignFormControl1.FluentDesignForm = this;
+            this.fluentDesignFormControl1.Location = new System.Drawing.Point(0, 0);
+            this.fluentDesignFormControl1.Manager = this.fluentFormDefaultManager1;
+            this.fluentDesignFormControl1.Name = "fluentDesignFormControl1";
+            this.fluentDesignFormControl1.Size = new System.Drawing.Size(916, 31);
+            this.fluentDesignFormControl1.TabIndex = 2;
+            this.fluentDesignFormControl1.TabStop = false;
+            // 
+            // fluentFormDefaultManager1
+            // 
+            this.fluentFormDefaultManager1.Form = this;
+            // 
+            // fdfcTrangHienThi
+            // 
+            this.fdfcTrangHienThi.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.fdfcTrangHienThi.Appearance.Options.UseBackColor = true;
+            this.fdfcTrangHienThi.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.fdfcTrangHienThi.Location = new System.Drawing.Point(260, 31);
+            this.fdfcTrangHienThi.Name = "fdfcTrangHienThi";
+            this.fdfcTrangHienThi.Size = new System.Drawing.Size(656, 509);
+            this.fdfcTrangHienThi.TabIndex = 0;
+            // 
+            // ceQuanLiNhanVien
+            // 
+            this.ceQuanLiNhanVien.Elements.AddRange(new DevExpress.XtraBars.Navigation.AccordionControlElement[] {
+            this.ceThemNhanVien,
+            this.ceSuaNhanVien,
+            this.ceXoaNhanVien});
+            this.ceQuanLiNhanVien.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("ceQuanLiNhanVien.ImageOptions.SvgImage")));
+            this.ceQuanLiNhanVien.Name = "ceQuanLiNhanVien";
+            this.ceQuanLiNhanVien.Text = "Quản Lí Nhân Viên";
+            // 
+            // ceThemNhanVien
+            // 
+            this.ceThemNhanVien.Name = "ceThemNhanVien";
+            this.ceThemNhanVien.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            this.ceThemNhanVien.Text = "Thêm Nhân Viên";
+            // 
+            // ceSuaNhanVien
+            // 
+            this.ceSuaNhanVien.Name = "ceSuaNhanVien";
+            this.ceSuaNhanVien.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            this.ceSuaNhanVien.Text = "Sửa Thông Tin Nhân Viên";
+            // 
+            // ceXoaNhanVien
+            // 
+            this.ceXoaNhanVien.Name = "ceXoaNhanVien";
+            this.ceXoaNhanVien.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            this.ceXoaNhanVien.Text = "Xóa Nhân Viên";
+            // 
+            // accordionControl1
+            // 
+            this.accordionControl1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.accordionControl1.Elements.AddRange(new DevExpress.XtraBars.Navigation.AccordionControlElement[] {
+            this.ceTrangChu,
+            this.ceDanhMucMonAn,
+            this.ceQuanLiNhanVien,
+            this.ceBaoCaoDoanhThu,
+            this.ceQuanLiKhoHang,
+            this.ceLichLam,
+            this.ceLienHe,
+            this.ceGioiThieu,
+            this.ceUpdate,
+            this.ceDangXuat});
+            this.accordionControl1.Location = new System.Drawing.Point(0, 31);
+            this.accordionControl1.Name = "accordionControl1";
+            this.accordionControl1.ScrollBarMode = DevExpress.XtraBars.Navigation.ScrollBarMode.Touch;
+            this.accordionControl1.Size = new System.Drawing.Size(260, 509);
+            this.accordionControl1.TabIndex = 1;
+            this.accordionControl1.ViewType = DevExpress.XtraBars.Navigation.AccordionControlViewType.HamburgerMenu;
+            // 
+            // ceTrangChu
+            // 
+            this.ceTrangChu.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("ceTrangChu.ImageOptions.SvgImage")));
+            this.ceTrangChu.Name = "ceTrangChu";
+            this.ceTrangChu.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            this.ceTrangChu.Text = "Trang Chủ";
+            // 
+            // ceDanhMucMonAn
+            // 
+            this.ceDanhMucMonAn.Elements.AddRange(new DevExpress.XtraBars.Navigation.AccordionControlElement[] {
+            this.ceThemMon,
+            this.ceSuaMon,
+            this.ceXoaMon});
+            this.ceDanhMucMonAn.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("ceDanhMucMonAn.ImageOptions.SvgImage")));
+            this.ceDanhMucMonAn.Name = "ceDanhMucMonAn";
+            this.ceDanhMucMonAn.Text = "Danh Mục Món Ăn";
+            // 
+            // ceThemMon
+            // 
+            this.ceThemMon.Name = "ceThemMon";
+            this.ceThemMon.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            this.ceThemMon.Text = "Thêm Món";
+            // 
+            // ceSuaMon
+            // 
+            this.ceSuaMon.Name = "ceSuaMon";
+            this.ceSuaMon.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            this.ceSuaMon.Text = "Sửa Món";
+            // 
+            // ceXoaMon
+            // 
+            this.ceXoaMon.Name = "ceXoaMon";
+            this.ceXoaMon.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            this.ceXoaMon.Text = "Xóa Món";
+            // 
+            // ceBaoCaoDoanhThu
+            // 
+            this.ceBaoCaoDoanhThu.Elements.AddRange(new DevExpress.XtraBars.Navigation.AccordionControlElement[] {
+            this.ceBaoCaoDT,
+            this.ceXemDT});
+            this.ceBaoCaoDoanhThu.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("ceBaoCaoDoanhThu.ImageOptions.SvgImage")));
+            this.ceBaoCaoDoanhThu.Name = "ceBaoCaoDoanhThu";
+            this.ceBaoCaoDoanhThu.Text = "Báo Cáo Doanh Thu";
+            // 
+            // ceBaoCaoDT
+            // 
+            this.ceBaoCaoDT.Name = "ceBaoCaoDT";
+            this.ceBaoCaoDT.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            this.ceBaoCaoDT.Text = "Báo Cáo Doanh Thu Ngày";
+            // 
+            // ceXemDT
+            // 
+            this.ceXemDT.Name = "ceXemDT";
+            this.ceXemDT.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            this.ceXemDT.Text = "Xem Doanh Thu";
+            // 
+            // ceQuanLiKhoHang
+            // 
+            this.ceQuanLiKhoHang.Elements.AddRange(new DevExpress.XtraBars.Navigation.AccordionControlElement[] {
+            this.ceBangKiemHang,
+            this.ceSPLoss,
+            this.ceBangXinNhapHang});
+            this.ceQuanLiKhoHang.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("ceQuanLiKhoHang.ImageOptions.SvgImage")));
+            this.ceQuanLiKhoHang.Name = "ceQuanLiKhoHang";
+            this.ceQuanLiKhoHang.Text = "Quản Lí Kho Hàng";
+            // 
+            // ceBangKiemHang
+            // 
+            this.ceBangKiemHang.Name = "ceBangKiemHang";
+            this.ceBangKiemHang.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            this.ceBangKiemHang.Text = "Bảng Kiểm Hàng";
+            // 
+            // ceSPLoss
+            // 
+            this.ceSPLoss.Name = "ceSPLoss";
+            this.ceSPLoss.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            this.ceSPLoss.Text = "Sản Phẩm Loss";
+            // 
+            // ceBangXinNhapHang
+            // 
+            this.ceBangXinNhapHang.Name = "ceBangXinNhapHang";
+            this.ceBangXinNhapHang.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            this.ceBangXinNhapHang.Text = "Bảng Xin Nhập Hàng Hóa";
+            // 
+            // ceLichLam
+            // 
+            this.ceLichLam.Elements.AddRange(new DevExpress.XtraBars.Navigation.AccordionControlElement[] {
+            this.ceThemLichLam,
+            this.ceSuaXoaLich,
+            this.ceTinhLuong});
+            this.ceLichLam.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("ceLichLam.ImageOptions.SvgImage")));
+            this.ceLichLam.Name = "ceLichLam";
+            this.ceLichLam.Text = "Lịch Làm";
+            // 
+            // ceThemLichLam
+            // 
+            this.ceThemLichLam.Name = "ceThemLichLam";
+            this.ceThemLichLam.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            this.ceThemLichLam.Text = "Thêm Lịch Làm";
+            // 
+            // ceSuaXoaLich
+            // 
+            this.ceSuaXoaLich.Name = "ceSuaXoaLich";
+            this.ceSuaXoaLich.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            this.ceSuaXoaLich.Text = "Sửa/ Xóa Lịch Làm";
+            // 
+            // ceTinhLuong
+            // 
+            this.ceTinhLuong.Name = "ceTinhLuong";
+            this.ceTinhLuong.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            this.ceTinhLuong.Text = "Tính Lương";
+            // 
+            // ceLienHe
+            // 
+            this.ceLienHe.Name = "ceLienHe";
+            this.ceLienHe.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            this.ceLienHe.Text = "Liên Hệ";
+            // 
+            // ceGioiThieu
+            // 
+            this.ceGioiThieu.Name = "ceGioiThieu";
+            this.ceGioiThieu.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            this.ceGioiThieu.Text = "Giới Thiệu";
+            // 
+            // ceUpdate
+            // 
+            this.ceUpdate.Name = "ceUpdate";
+            this.ceUpdate.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            this.ceUpdate.Text = "Update";
+            // 
+            // ceDangXuat
+            // 
+            this.ceDangXuat.Name = "ceDangXuat";
+            this.ceDangXuat.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            this.ceDangXuat.Text = "Đăng Xuất";
+            // 
+            // accordionControlElement4
+            // 
+            this.accordionControlElement4.Name = "accordionControlElement4";
+            this.accordionControlElement4.Text = "Element4";
+            // 
+            // accordionControlElement3
+            // 
+            this.accordionControlElement3.Name = "accordionControlElement3";
+            this.accordionControlElement3.Text = "Element3";
+            // 
+            // accordionControlElement2
+            // 
+            this.accordionControlElement2.Expanded = true;
+            this.accordionControlElement2.Name = "accordionControlElement2";
+            this.accordionControlElement2.Text = "Element2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(916, 540);
+            this.ControlContainer = this.fdfcTrangHienThi;
+            this.Controls.Add(this.fdfcTrangHienThi);
+            this.Controls.Add(this.accordionControl1);
+            this.Controls.Add(this.fluentDesignFormControl1);
+            this.FluentDesignFormControl = this.fluentDesignFormControl1;
+            this.IconOptions.Image = ((System.Drawing.Image)(resources.GetObject("Form1.IconOptions.Image")));
+            this.Name = "Form1";
+            this.NavigationControl = this.accordionControl1;
+            this.Text = "Quản Lí Nhà Hàng Sukiya";
+            ((System.ComponentModel.ISupportInitialize)(this.fluentDesignFormControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fluentFormDefaultManager1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.accordionControl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private DevExpress.XtraBars.FluentDesignSystem.FluentDesignFormControl fluentDesignFormControl1;
+        private DevExpress.XtraBars.FluentDesignSystem.FluentFormDefaultManager fluentFormDefaultManager1;
+        private DevExpress.XtraBars.FluentDesignSystem.FluentDesignFormContainer fdfcTrangHienThi;
+        private DevExpress.XtraBars.Navigation.AccordionControl accordionControl1;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement ceQuanLiNhanVien;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement ceDanhMucMonAn;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement4;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement3;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement2;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement ceThemNhanVien;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement ceXoaNhanVien;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement ceThemMon;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement ceSuaMon;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement ceXoaMon;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement ceQuanLiKhoHang;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement ceBangKiemHang;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement ceSPLoss;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement ceBangXinNhapHang;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement ceLienHe;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement ceGioiThieu;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement ceUpdate;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement ceDangXuat;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement ceBaoCaoDoanhThu;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement ceBaoCaoDT;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement ceXemDT;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement ceLichLam;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement ceThemLichLam;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement ceSuaXoaLich;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement ceTinhLuong;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement ceTrangChu;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement ceSuaNhanVien;
+    }
+}
+
---
+++ Sukiya/Form1.cs
@@ -0,0 +1,21 @@
+﻿using DevExpress.XtraBars;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sukiya
+{
+    public partial class Form1 : DevExpress.XtraBars.FluentDesignSystem.FluentDesignForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
